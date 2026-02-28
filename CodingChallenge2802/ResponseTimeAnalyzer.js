5588f4e514533ae9d83e3d2bce9a06c957cabb6e
Chaganti <neeharikach1308@gmail.com>
2026-02-28 23:14:48 +0530

    Added  ResponseTimeAnalyzer.js

---
+++ CodingChallenge2802/ResponseTimeAnalyzer.js
@@ -0,0 +1,65 @@
+/**
+ * Response Time SLA Analyzer
+As a performance tester, you collect API response times in milliseconds. 
+Write a JavaScript program using a while loop that 
+analyzes an array of response times and 
+prints a performance report with min, max, average, and how many responses breached the SLA threshold (> 500ms). 
+Use comparison operators for min/max tracking.
+ */
+
+/**
+ * responseTimes = [120, 230, 450, 510, 180, 620], SLA_LIMIT = 500
+ */
+
+/**
+ * Total Requests: 6 
+ * Min Response: 120ms 
+ * Max Response: 620ms 
+ * SLA Breaches: 2 (33.33%) 
+ * Overall Status: ❌ SLA VIOLATED
+ */
+
+const responseTimes = [120, 230, 450, 510, 180, 620, 400, 280, 500, 333, 444];
+const SLA_LIMIT = 500;
+
+const totalRequests = responseTimes.length;
+let minResponseTime = responseTimes[0];
+let maxResponseTime = responseTimes[0];
+
+let sum = 0;
+let breachTickets = 0; 
+
+let i = 0;
+while(i < totalRequests){
+	currentResponseTime = responseTimes[i];
+	if(currentResponseTime < minResponseTime){
+		currentResponseTime = minResponseTime;
+	}
+	if(currentResponseTime > maxResponseTime){
+		maxResponseTime = currentResponseTime;
+	}
+	sum = sum + currentResponseTime;
+	
+	if(currentResponseTime > SLA_LIMIT){
+		breachTickets = breachTickets + 1;
+	}
+	i++;
+}
+
+let avgResponseTime = sum/totalRequests;
+
+let breachPercentage = (breachTickets/totalRequests) * 100;
+
+
+console.log("*****Test Report*****");
+console.log("Total Requests :", totalRequests);
+console.log("Min response Time :", minResponseTime);
+console.log("Max response Time :", maxResponseTime);
+console.log("SLA Breaches : ", breachTickets + "(" + breachPercentage + "%)" );
+
+if (breachTickets > 0) {
+    console.log("Overall Status: ❌ SLA VIOLATED");
+} else {
+    console.log("Overall Status: ✅ SLA MET");
+}
+
