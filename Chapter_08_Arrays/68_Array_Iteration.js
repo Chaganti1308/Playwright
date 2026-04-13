9faa6a32d144a1eb7be86b67d5b24734c063d6d2
Chaganti <neeharikach1308@gmail.com>
2026-04-13 16:01:19 +0530

    Updated code

--- Chapter_08_Arrays/68_Array_Iteration.js
+++ Chapter_08_Arrays/68_Array_Iteration.js
@@ -2,32 +2,34 @@
 
 let tests = ['login','checkout','search'];
 
+console.log("-----------For()-------------");
 for(let i =1; i<tests.length;i++){
     console.log(tests[i]);
     
 }
 
-console.log("------------------------");
+console.log("----------For of()--------------");
 // for .. of -- cleanest for values
 for(let test of tests){
     console.log(test);
     
 }
 
-console.log("------------------------");
+console.log("------------forEach()------------");
 // for .. each -- no return value
 tests.forEach((test,index)  => {
     console.log(`${index} : ${test}`);
     
 });
 
-console.log("------------------------");
+console.log("-----------enties()-------------");
 
 // entries() -- index + value
 for(let[i,test] of tests.entries()){
     console.log(i,test);
     
-}console.log("------------------------");
+}
+console.log("-----------for in()-------------");
 
 let students = ['senthil','Ajay','krish','rahul','rekha'];
 
